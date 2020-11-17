7a5268c20d03a73408fbc9dad87077a7db6ff538
Isaias Majorkey <isaiasnhantumbo54@gmail.com>
2020-11-17 18:19:28 +0200

    :tada: add product form fields

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const routes = express.Router();
+const ProductsController = require("./app/controllers/ProductsController");
 
-routes.get("/", function (req, res) {
-  return res.send("ok");
+routes.get('/', function(req, res){
+  return res.render("layout.njk")
+})
+
+routes.get("/products/create", ProductsController.create);
+
+routes.get("/ads/create  ", function (req, res) {
+  return res.redirect("/products/create");
 });
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -18,6 +18,6 @@ nunjucks.configure("src/app/views", {
   noCache: true,
 });
 
-server.listen(3333, () => {
+server.listen(3000, () => {
   console.log("Server is running");
 });
