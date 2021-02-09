870c4f7bde485c1bf9633068ce52fc6cad7ed52c
Isaias Nhantumbo Junior <isaiasnhantumbo54@gmail.com>
2021-02-09 10:37:45 +0200

    :tada: applied MVC pattern in Home

--- src/app/controllers/HomeController.js
+++ src/app/controllers/HomeController.js
@@ -1,30 +1,37 @@
-const { formatPrice, date } = require("../../lib/utils");
-
-const Product = require("../models/Product");
+const Product = require('../models/Product');
+const { formatPrice, date } = require('../../lib/utils');
 
 module.exports = {
   async index(req, res) {
-    let results = await Product.all();
-    const products = results.rows;
+    const products = await Product.findAll();
 
-    if (!products) return res.send("Product not Found");
+    if (!products) return res.send('Product not Found');
 
     async function getImage(productId) {
-        let results = await Product.files(productId)
-        let files = results.rows.map(file => `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`)
-        
-        return files[0]
+      let files = await Product.files(productId);
+      
+      files = files.map(
+        (file) =>
+          `${req.protocol}://${req.headers.host}${file.path.replace(
+            'public',
+            ''
+          )}`
+      );
+
+      return files[0];
     }
 
-    const productsPromise = products.map(async product => {
-        product.img =  await getImage(product.id);
+    const productsPromise = products
+      .map(async (product) => {
+        product.img = await getImage(product.id);
         product.oldPrice = formatPrice(product.old_price);
         product.price = formatPrice(product.price);
         return product;
-      }).filter((product, index) => (index > 2 ? false : true));
+      })
+      .filter((product, index) => (index > 2 ? false : true));
 
     const lastAdded = await Promise.all(productsPromise);
 
-    return res.render("home/index", { products: lastAdded });
+    return res.render('home/index', { products: lastAdded });
   },
 };
